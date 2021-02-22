252cbc9d0a9989f29d932b96d32ad3096b061add
treasgu <treasgu@gmail.com>
2021-02-22 16:36:15 +0100

    Create ui_panel_relative.py
    Init

---
+++ ui_panel_relative.py
@@ -0,0 +1,46 @@
+import bpy
+
+
+class RelativePositionPanel(bpy.types.Panel):
+    """Creates a Panel in the Object properties window"""
+    bl_label = "Mi paneluku"
+    bl_idname = "OBJECT_My_Rel_pos"
+    bl_space_type = 'PROPERTIES'
+    bl_region_type = 'WINDOW'
+    bl_context = "object"
+
+    def draw(self, context):
+        layout = self.layout
+
+        obj = context.object
+
+        row = layout.row()
+        row.label(text="Show parent relative position", icon='WORLD_DATA')
+
+        row = layout.row()
+        row.label(text="Active object is: " + obj.name)
+        row = layout.row()
+        row.prop(obj, "name")
+        
+        vector = obj.parent.matrix_world.translation - obj.matrix_world.translation
+        
+        row = layout.row()
+        row.label(text="X: " + str(vector.x))
+        row.label(text="Y: " + str(vector.y))
+        row.label(text="Z: " + str(vector.z))
+        
+
+        row = layout.row()
+        row.operator("mesh.primitive_cube_add")
+
+
+def register():
+    bpy.utils.register_class(RelativePositionPanel)
+
+
+def unregister():
+    bpy.utils.unregister_class(RelativePositionPanel)
+
+
+if __name__ == "__main__":
+    register()
